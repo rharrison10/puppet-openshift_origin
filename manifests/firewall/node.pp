ce81f4bca90153f21199739b2f911de16ef614df
Russell Harrison <rharriso@redhat.com>
2014-04-23 10:11:12 -0400

    Setup rhc-app-comm iptables chain using lokkit

--- manifests/firewall/node.pp
+++ manifests/firewall/node.pp
@@ -1,5 +1,41 @@
+# == Class: openshift_origin::firewall::node
+#
+# Setup the required firewall rules and IPTables chains for a working OpenShift node.
+#
+# === Parameters
+#
+# None
+#
+# === Examples
+#
+#  include ::openshift_origin::firewall::node
+#
+# === Copyright
+#
+# Copyright 2014 Red Hat, Inc.
+#
+# === License
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 class openshift_origin::firewall::node {
   lokkit::ports { 'Node Port Range':
     tcpPorts => ['35531-65535'],
+  }
+
+  lokkit::custom { 'rhc-app-comm_chain':
+    ensure => present,
+    type   => 'ipv4',
+    source => 'puppet:///openshift_origin/node/node_iptables.txt',
   }
 }
